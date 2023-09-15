7c09ce06c4e7eea64b0637d51ef3a8680d9a6eb0
Shiota Teito <Lasselaflair@gmail.com>
2023-09-15 12:53:30 +0200

    update

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -3,26 +3,36 @@ import { useApiHandler } from '../services/APIHandler'
 
 export default function HomePage() {
 
-    const [data, setData] = useState(null);
+    const [ initData, getNextPage, games, loading, error ] = useApiHandler();
 
-    const [ apiHandler, loading, error ] = useApiHandler();
-
-    const getData = async () => {
-        const response = await apiHandler({path: '/games'});
-        setData(response.data);
-    }
+    // const getData = async () => {
+    //     const response = await apiHandler({path: '/games'});
+    //     setData(response.results);
+    // }
 
     useEffect(() => {
-        getData();
-    }, []);
+        initData({path: '/games'});
+        console.log(games);
+        
 
-    if (loading) { return <p>Loading...</p> }
-    if (error) { return <p>There was an error loading the data.</p> }
+    }, [games]);
+
+    if (loading !== false) { return <p>Loading...</p> }
+    if (error) { return <p>There was an error loading the data. <br/> {error.message} </p> }
     
     return (
         <>
             <h1>Home Page</h1>
-            <p>{data}</p>
+            <ul>
+                {games.map((game) => (
+                    <li key={game.id}>
+                        <h2>{game.name}</h2>
+                        <p>{game.description_raw}</p>
+                        <img src={game.background_image} alt={game.name} />
+                    </li>
+                ))}
+            </ul>
+
         </>
     );
 }
--- src/services/APIHandler.js
+++ src/services/APIHandler.js
@@ -1,29 +1,60 @@
 import { useState } from 'react';
 import axios from 'axios';
 
+const rawgAPI = axios.create({
+    baseURL: 'https://api.rawg.io/api',
+    headers: { 'Content-Type': 'application/json', 'Accept': 'application/json' },
+    params: { key: '64be55ab7ca3444fa2b80754b9ee90c4' }
+});
+
+class DataType {
+    constructor(data, path, count, pagesLoaded, nextPage, prevPage) {
+        {
+            this.path = path;
+            this.count = count;
+            this.pagesLoaded = pagesLoaded;
+            this.nextPage = nextPage;
+            this.prevPage = prevPage;
+            this.data = [{ ...data }];
+        }
+    }
+}
+
 
 export function useApiHandler() {
+    const [data, setData] = useState(null);
     const [loading, setLoading] = useState(false);
     const [error, setError] = useState(null);
 
-    const apiHandler = async ({path, page = 1}) => {
-
-        const headers = {
-            'Content-Type': 'application/json'
-        };
-
-        const params = {
-            key: '64be55ab7ca3444fa2b80754b9ee90c4'
-        };
-
+    async function initData({ initPath = '/games' }){
+        if ( data ) { return; }
         try {
             setLoading(true);
-            const response = await axios.get({
-                baseURL: 'https://api.rawg.io/api',
-                url: path,
-                headers: headers,
-                params: params
-            });
+            const response = await rawgAPI.get(initPath);
+            const data = new DataType(
+                initPath,
+                response.data.results,
+                response.data.count,
+                1,
+                response.data.next,
+                response.data.previous
+            );
+            console.log(response.data.results);
+            setData(response.data.results);
+            console.log(data);
+            setLoading(false);
+        } catch (error) {
+            setLoading(false);
+            setError(error);
+            throw error;
+        }
+    };
+
+
+    async function apiHandler({path, page = 1}){
+        try {
+            setLoading(true);
+            const response = await rawgAPI.get(path);
             setLoading(false);
             return response.data;
         } catch (error) {
@@ -33,5 +64,33 @@ export function useApiHandler() {
         }
     };
 
-    return [apiHandler, loading, error];
+    function updateData(newData){
+        setData(
+            {
+                data: [...data, newData],
+                pagesLoaded: data.pagesLoaded + 1,
+                nextPage: data.nextPage,
+                prevPage: data.prevPage
+            }
+        );
+    }
+
+
+
+    async function getNextPage(){
+        try {
+            setLoading(true);
+            console.log( 'Getting Next page:' + data);
+            const response = await rawgAPI.get(data.nextPage)
+                .then(response => response.data);
+            updateData(response.results);
+            setLoading(false);
+        } catch (error) {
+            setLoading(false);
+            setError(error);
+            throw error;
+        }
+    }
+
+    return [ initData, getNextPage, data, loading, error];
 }
